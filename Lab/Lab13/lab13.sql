030ad78de13fd38242098ff240286126ec2968ff
Anderson Lam Wei Cheng <65696858+lamweicheng@users.noreply.github.com>
2022-12-08 21:38:19 -0800

    Add files via upload

---
+++ Lab/Lab13/lab13.sql
@@ -0,0 +1,38 @@
+.read data.sql
+
+
+CREATE TABLE bluedog AS
+  SELECT color, pet FROM students WHERE color = "blue" AND pet = 'dog' ;
+
+CREATE TABLE bluedog_songs AS
+  SELECT color, pet, song FROM students WHERE color= 'blue' AND pet = 'dog';
+
+
+CREATE TABLE smallest_int_having AS
+  SELECT time, smallest FROM students GROUP BY smallest HAVING count(smallest) = 1 ;
+
+
+CREATE TABLE matchmaker AS
+  SELECT student1.pet, student1.song, student1.color, student2.color FROM students AS student1, students AS student2 WHERE student1.time < student2. time 
+  AND student1.pet = student2.pet AND student1.song = student2.song ;
+
+
+CREATE TABLE sevens AS
+  SELECT a.seven FROM students AS a, numbers AS b where a.number = 7 AND b."7" = "True" AND a.time = b.time;
+
+
+CREATE TABLE average_prices AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+
+
+CREATE TABLE lowest_prices AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+
+
+CREATE TABLE shopping_list AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+
+
+CREATE TABLE total_bandwidth AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+
